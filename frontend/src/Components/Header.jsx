55e332586cbb0ace146233cd3ff2351757f78878
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2022-05-16 15:22:12 +0100

    Update app

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { Heading, Flex, Divider } from "@chakra-ui/core";
+import { Heading, Flex, Divider } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding="0.5rem"
-      bg="gray.400"
-    >
-      <Flex align="center" mr={5}>
-        <Heading as="h1" size="sm">Todos</Heading>
-        <Divider />
+      <Flex
+          as="nav"
+          align="center"
+          justify="space-between"
+          wrap="wrap"
+          padding="0.5rem"
+          bg="gray.400"
+      >
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="sm">Todos</Heading>
+          <Divider />
+        </Flex>
       </Flex>
-    </Flex>
   );
 };
 
