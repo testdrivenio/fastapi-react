0153368267f9b747d9c4e408f35e62d46f6660f7
Greg Anderson <greg.anderson.cs@gmail.com>
2023-12-11 12:49:58 -0700

    Upgrade package versions
    The chakra-ui package upgraded and isn't backward compatible, so this
    makes the necessary changes to upgrade. You should now be able to
    `npm install` and `npm start` without needing to make any changes, and
    the frontend will work. Also fixed some formatting.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { render } from 'react-dom';
-import { ChakraProvider } from "@chakra-ui/core";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import Header from "./Components/Header";
 import Todos from "./Components/Todos";
@@ -11,8 +11,9 @@ function App() {
       <Header />
       <Todos />
     </ChakraProvider>
-  )
+  );
 }
 
-const rootElement = document.getElementById("root")
-render(<App />, rootElement)
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<App />);
